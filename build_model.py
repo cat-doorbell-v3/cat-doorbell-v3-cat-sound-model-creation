892556a399ff33f65907906b6c31f11e03b9550d
gamename <tennis.n.smith@gmail.com>
2024-03-01 12:31:13 -0600

    moved common utils to a lib to make way for testing

--- build_model.py
+++ build_model.py
@@ -1,11 +1,3 @@
-import os
-import shutil
-import zipfile
-
-import librosa
-import matplotlib.pyplot as plt
-import numpy as np
-import tensorflow as tf
 from keras.regularizers import l2
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import EarlyStopping
@@ -15,186 +7,25 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
 
 import constants
-
-
-def add_random_noise(data, noise_level=0.005):
-    """
-    Adds random noise to the data.
-
-    Args:
-        data (numpy.ndarray): The input data (spectrogram).
-        noise_level (float): The amplitude of the noise to add (relative to data's range).
-
-    Returns:
-        numpy.ndarray: The data with added random noise.
-    """
-    noise = np.random.randn(*data.shape) * noise_level
-    augmented_data = data + noise
-    return np.clip(augmented_data, -np.inf, np.inf)  # Ensure values stay within a valid range
-
-
-def augment_data(X, y):
-    """
-    Augments the dataset by applying random transformations.
-
-    Args:
-        X (numpy.ndarray): The input features (spectrograms).
-        y (numpy.ndarray): The target labels.
-
-    Returns:
-        numpy.ndarray: The augmented input features.
-        numpy.ndarray: The (unmodified) target labels.
-    """
-    X_augmented = np.array([add_random_noise(x) for x in X])
-    return X_augmented, y
-
-def unzip_file(zip_file_path, extract_to_path):
-    """
-    Unzips a ZIP file to a specified location.
-
-    Args:
-    zip_file_path (str): The path to the ZIP file.
-    extract_to_path (str): The destination directory where the contents will be extracted.
-
-    Returns:
-    None
-    """
-    # Ensure the destination directory exists, create if it doesn't
-    if not os.path.exists(extract_to_path):
-        os.makedirs(extract_to_path)
-
-    # Open the zip file
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        # Extract all the contents into the destination directory
-        zip_ref.extractall(extract_to_path)
-    print(f"File extracted to {extract_to_path}")
-
-
-def remove_directories(dir_list):
-    """
-    Remove a list of directories located in the /tmp directory.
-
-    Parameters:
-    - dir_list (list of str): A list of directory names to be removed.
-
-    Returns:
-    - None
-    """
-    base_path = "/tmp"
-    for dir_name in dir_list:
-        dir_path = os.path.join(base_path, dir_name)
-        try:
-            shutil.rmtree(dir_path)
-            print(f"Directory {dir_path} removed successfully.")
-        except FileNotFoundError:
-            print(f"Directory {dir_path} does not exist.")
-        except Exception as e:
-            print(f"Error removing directory {dir_path}: {e}")
-
-
-def audio_to_spectrogram(file_path, n_mels=constants.N_MELS, n_fft=constants.N_FFT, hop_length=constants.HOP_LENGTH,
-                         max_pad_len=None):
-    y, sr = librosa.load(file_path, sr=constants.SAMPLING_RATE, duration=constants.AUDIO_DURATION)
-    spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, n_fft=n_fft, hop_length=hop_length)
-    spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
-
-    # Pad or truncate the spectrogram to ensure a consistent shape
-    pad_width = max_pad_len - spectrogram.shape[1]
-    if pad_width > 0:  # Pad the spectrogram
-        spectrogram = np.pad(spectrogram, pad_width=((0, 0), (0, pad_width)), mode='constant')
-    else:  # Truncate the spectrogram
-        spectrogram = spectrogram[:, :max_pad_len]
-
-    return spectrogram
-
-
-def find_max_spectrogram_length(dataset_path, categories, n_mels=constants.N_MELS, n_fft=constants.N_FFT,
-                                hop_length=constants.HOP_LENGTH):
-    max_len = 0
-    for category in categories:
-        category_path = os.path.join(dataset_path, category)
-        for file in os.listdir(category_path):
-            if file.endswith(".wav"):
-                file_path = os.path.join(category_path, file)
-                y, sr = librosa.load(file_path, sr=constants.SAMPLING_RATE, duration=constants.AUDIO_DURATION)
-                spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, n_fft=n_fft,
-                                                             hop_length=hop_length)
-                max_len = max(max_len, spectrogram.shape[1])
-    return max_len
-
-
-def load_dataset(dataset_path, categories, max_pad_len):
-    X, y = [], []
-    for label, category in enumerate(categories):
-        category_path = os.path.join(dataset_path, category)
-        for file in os.listdir(category_path):
-            if file.endswith(".wav"):
-                file_path = os.path.join(category_path, file)
-                spectrogram = audio_to_spectrogram(file_path, max_pad_len=max_pad_len)
-                X.append(spectrogram)
-                y.append(label)
-    return np.array(X), np.array(y)
-
-
-# Convert the model to the TensorFlow Lite format with quantization
-def convert_to_tflite(model, X_train, filename):
-    converter = tf.lite.TFLiteConverter.from_keras_model(model)
-    converter.optimizations = [tf.lite.Optimize.DEFAULT]
-
-    def representative_dataset_gen():
-        for i in range(100):
-            # Ensure the sample input data is cast to FLOAT32
-            yield [X_train[i].reshape(1, *X_train[i].shape).astype(np.float32)]
-
-    converter.representative_dataset = representative_dataset_gen
-    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-    converter.inference_input_type = tf.int8
-    converter.inference_output_type = tf.int8
-
-    tflite_model_quant = converter.convert()
-    open(filename, "wb").write(tflite_model_quant)
-
-
-def plot_model_fit(history_data):
-    # Plotting training & validation accuracy values
-    plt.figure(figsize=(12, 6))
-    plt.subplot(1, 2, 1)
-    plt.plot(history_data['accuracy'])
-    plt.plot(history_data['val_accuracy'])
-    plt.title('Model Accuracy')
-    plt.ylabel('Accuracy')
-    plt.xlabel('Epoch')
-    plt.legend(['Train', 'Test'], loc='upper left')
-
-    # Plotting training & validation loss values
-    plt.subplot(1, 2, 2)
-    plt.plot(history_data['loss'])
-    plt.plot(history_data['val_loss'])
-    plt.title('Model Loss')
-    plt.ylabel('Loss')
-    plt.xlabel('Epoch')
-    plt.legend(['Train', 'Test'], loc='upper left')
-
-    plt.tight_layout()
-    plt.show()
+import utils
 
 
 def main():
-    remove_directories([constants.MODEL_DATASET])
+    utils.remove_directories([constants.MODEL_DATASET])
 
-    unzip_file(constants.MODEL_DATASET_ZIP, '/tmp')
+    utils.unzip_file(constants.MODEL_DATASET_ZIP, '/tmp')
 
     # Find the maximum spectrogram length
-    max_pad_len = find_max_spectrogram_length(constants.MODEL_DATASET_PATH, constants.MODEL_DATASET_CATEGORIES)
+    max_pad_len = utils.find_max_spectrogram_length(constants.MODEL_DATASET_PATH, constants.MODEL_DATASET_CATEGORIES)
     print(f"Maximum pad length: {max_pad_len}")
 
     # Load and pad/truncate dataset
-    X, y = load_dataset(constants.MODEL_DATASET_PATH, constants.MODEL_DATASET_CATEGORIES, max_pad_len)
+    X, y = utils.load_dataset(constants.MODEL_DATASET_PATH, constants.MODEL_DATASET_CATEGORIES, max_pad_len)
 
     # Ensure each spectrogram has the same second dimension
     X = X.reshape(*X.shape, 1)  # Add channel dimension for CNN input
 
-    X_augmented, y_augmented = augment_data(X, y)
+    X_augmented, y_augmented = utils.augment_data(X, y)
     # Split the dataset into training and validation sets
     X_train, X_val, y_train, y_val = train_test_split(X_augmented, y_augmented, test_size=0.2, random_state=42)
 
@@ -204,8 +35,6 @@ def main():
     y_train = to_categorical(y_train, num_classes)
     y_val = to_categorical(y_val, num_classes)
 
-    # Assuming binary classification (cat vs noise) and using 'binary_crossentropy' loss
-    num_classes = 2  # Update if you have more classes
     input_shape = X_train.shape[1:]  # Should be (spectrogram_height, spectrogram_width, 1)
 
     model = Sequential([
@@ -243,9 +72,9 @@ def main():
     )
 
     # Train the model
-    plot_model_fit(history.history)
+    utils.plot_model_fit(history.history)
 
-    convert_to_tflite(model, X_train, constants.MODEL_OUTPUT_FILE_NAME)
+    utils.convert_to_tflite(model, X_train, constants.MODEL_OUTPUT_FILE_NAME)
 
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,173 @@
+import os
+import shutil
+import zipfile
+
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+import constants
+
+
+def add_random_noise(data, noise_level=0.005):
+    """
+    Adds random noise to the data.
+
+    Args:
+        data (numpy.ndarray): The input data (spectrogram).
+        noise_level (float): The amplitude of the noise to add (relative to data's range).
+
+    Returns:
+        numpy.ndarray: The data with added random noise.
+    """
+    noise = np.random.randn(*data.shape) * noise_level
+    augmented_data = data + noise
+    return np.clip(augmented_data, -np.inf, np.inf)  # Ensure values stay within a valid range
+
+
+def augment_data(X, y):
+    """
+    Augments the dataset by applying random transformations.
+
+    Args:
+        X (numpy.ndarray): The input features (spectrograms).
+        y (numpy.ndarray): The target labels.
+
+    Returns:
+        numpy.ndarray: The augmented input features.
+        numpy.ndarray: The (unmodified) target labels.
+    """
+    X_augmented = np.array([add_random_noise(x) for x in X])
+    return X_augmented, y
+
+
+def unzip_file(zip_file_path, extract_to_path):
+    """
+    Unzips a ZIP file to a specified location.
+
+    Args:
+    zip_file_path (str): The path to the ZIP file.
+    extract_to_path (str): The destination directory where the contents will be extracted.
+
+    Returns:
+    None
+    """
+    # Ensure the destination directory exists, create if it doesn't
+    if not os.path.exists(extract_to_path):
+        os.makedirs(extract_to_path)
+
+    # Open the zip file
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        # Extract all the contents into the destination directory
+        zip_ref.extractall(extract_to_path)
+    print(f"File extracted to {extract_to_path}")
+
+
+def remove_directories(dir_list):
+    """
+    Remove a list of directories located in the /tmp directory.
+
+    Parameters:
+    - dir_list (list of str): A list of directory names to be removed.
+
+    Returns:
+    - None
+    """
+    base_path = "/tmp"
+    for dir_name in dir_list:
+        dir_path = os.path.join(base_path, dir_name)
+        try:
+            shutil.rmtree(dir_path)
+            print(f"Directory {dir_path} removed successfully.")
+        except FileNotFoundError:
+            print(f"Directory {dir_path} does not exist.")
+        except Exception as e:
+            print(f"Error removing directory {dir_path}: {e}")
+
+
+def audio_to_spectrogram(file_path, n_mels=constants.N_MELS, n_fft=constants.N_FFT, hop_length=constants.HOP_LENGTH,
+                         max_pad_len=None):
+    y, sr = librosa.load(file_path, sr=constants.SAMPLING_RATE, duration=constants.AUDIO_DURATION)
+    spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, n_fft=n_fft, hop_length=hop_length)
+    spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
+
+    # Pad or truncate the spectrogram to ensure a consistent shape
+    pad_width = max_pad_len - spectrogram.shape[1]
+    if pad_width > 0:  # Pad the spectrogram
+        spectrogram = np.pad(spectrogram, pad_width=((0, 0), (0, pad_width)), mode='constant')
+    else:  # Truncate the spectrogram
+        spectrogram = spectrogram[:, :max_pad_len]
+
+    return spectrogram
+
+
+def find_max_spectrogram_length(dataset_path, categories, n_mels=constants.N_MELS, n_fft=constants.N_FFT,
+                                hop_length=constants.HOP_LENGTH):
+    max_len = 0
+    for category in categories:
+        category_path = os.path.join(dataset_path, category)
+        for file in os.listdir(category_path):
+            if file.endswith(".wav"):
+                file_path = os.path.join(category_path, file)
+                y, sr = librosa.load(file_path, sr=constants.SAMPLING_RATE, duration=constants.AUDIO_DURATION)
+                spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, n_fft=n_fft,
+                                                             hop_length=hop_length)
+                max_len = max(max_len, spectrogram.shape[1])
+    return max_len
+
+
+def load_dataset(dataset_path, categories, max_pad_len):
+    X, y = [], []
+    for label, category in enumerate(categories):
+        category_path = os.path.join(dataset_path, category)
+        for file in os.listdir(category_path):
+            if file.endswith(".wav"):
+                file_path = os.path.join(category_path, file)
+                spectrogram = audio_to_spectrogram(file_path, max_pad_len=max_pad_len)
+                X.append(spectrogram)
+                y.append(label)
+    return np.array(X), np.array(y)
+
+
+# Convert the model to the TensorFlow Lite format with quantization
+def convert_to_tflite(model, X_train, filename):
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+    def representative_dataset_gen():
+        for i in range(100):
+            # Ensure the sample input data is cast to FLOAT32
+            yield [X_train[i].reshape(1, *X_train[i].shape).astype(np.float32)]
+
+    converter.representative_dataset = representative_dataset_gen
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    converter.inference_input_type = tf.int8
+    converter.inference_output_type = tf.int8
+
+    tflite_model_quant = converter.convert()
+    open(filename, "wb").write(tflite_model_quant)
+
+
+def plot_model_fit(history_data):
+    # Plotting training & validation accuracy values
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.plot(history_data['accuracy'])
+    plt.plot(history_data['val_accuracy'])
+    plt.title('Model Accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+
+    # Plotting training & validation loss values
+    plt.subplot(1, 2, 2)
+    plt.plot(history_data['loss'])
+    plt.plot(history_data['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+
+    plt.tight_layout()
+    plt.show()
